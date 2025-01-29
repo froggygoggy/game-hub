25e94a2d219669e2e265b792bf57e5405e28fa85
Gustav <71986914+froggygoggy@users.noreply.github.com>
2025-01-29 16:24:33 +0100

    install chakra ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
+import { ColorModeButton } from "@/components/ui/color-mode";
 
 function App() {
-  return <Button>Click Me</Button>;
+  return (
+    <>
+      <Button variant="surface">Click Me</Button>
+      <ColorModeButton />
+    </>
+  );
   // return <button type="button">Click Me!</button>;
 }
 
