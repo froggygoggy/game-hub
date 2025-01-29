b63ef6cd34443ac8c174b14969f5b1109ff15f93
Gustav <71986914+froggygoggy@users.noreply.github.com>
2025-01-29 18:50:32 +0100

    7.7+7.8 Color Mode Button

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,13 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
+import { ColorModeButton } from "./ui/color-mode";
 
 const NavBar = () => {
   return (
     <HStack>
       <Image src={logo} boxSize="50px" alt="Logo" />
       <Text>NavBar</Text>
+      <ColorModeButton />
     </HStack>
   );
 };
---
+++ src/theme.ts
@@ -0,0 +1,9 @@
+import { createSystem, defaultConfig } from "@chakra-ui/react";
+
+const system = createSystem(defaultConfig, {
+  theme: {
+    tokens: {
+      colors: {},
+    },
+  },
+});
