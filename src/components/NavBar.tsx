1a33670cf8b377e5fabc28fd6b9f29405615664d
Gustav <71986914+froggygoggy@users.noreply.github.com>
2025-01-30 20:37:46 +0100

    7.11 Building Game Cards

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,5 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
-import { ColorModeButton } from "./ui/color-mode";
 import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
--- src/theme.ts
+++ src/theme.ts
@@ -1,9 +1,10 @@
-import { createSystem, defaultConfig } from "@chakra-ui/react";
+// this is going to be my own theme in the future
+// import { createSystem, defaultConfig } from "@chakra-ui/react";
 
-const system = createSystem(defaultConfig, {
-  theme: {
-    tokens: {
-      colors: {},
-    },
-  },
-});
+// const system = createSystem(defaultConfig, {
+//   theme: {
+//     tokens: {
+//       colors: {},
+//     },
+//   },
+// });
